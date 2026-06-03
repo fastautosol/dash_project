67ab294da5011dad9c99470dd8312be0a4b5660c
fastautosol <fastautosol@gmail.com>
2026-06-03 10:33:44 +0200

    Update main.py

--- main.py
+++ main.py
@@ -7,8 +7,6 @@ from fastapi.staticfiles import StaticFiles
 from fastapi.middleware.wsgi import WSGIMiddleware
 
 import apis.crm_shopify_api as crm_shopify_api
-import apis.bybit_api as bybit
-import apis.cryptonews_api as cryptonews_api
 import apis.kraken_api as kraken
 import apis.lufthansa_api as lufthansa_api
 import apis.serper_places_api as serper_places
