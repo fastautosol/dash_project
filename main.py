c2b369ae9babe481a875e74145b318d4912770ba
fastautosol <fastautosol@gmail.com>
2026-07-07 14:39:56 +0200

    Update main.py

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ import apis.youtube_api as youtube_api
 
 # ----- 1. Initialize Dash -----
 app = dash.Dash(__name__, use_pages=True, suppress_callback_exceptions=True,  
-    meta_tags=[{"name": "impact-site-verification",  "content": "d9a897f9-f49b-48be-9813-8a019d18dc6c"}],
+    meta_tags=[{"name": "impact-site-verification",  "content": "d73bf68a-2290-414c-858c-fa9dadcd2fd9"}],
     external_stylesheets=[dbc.themes.DARKLY, "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/7.0.1/css/all.min.css"],
     external_scripts=["https://unpkg.com/lightweight-charts@5.2.0/dist/lightweight-charts.standalone.production.js"])
 
