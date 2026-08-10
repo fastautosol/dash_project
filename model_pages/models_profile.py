f15d72e95c6d216b37009db4d7db9685a513cdb2
fastautosol <fastautosol@gmail.com>
2026-08-10 17:23:19 +0200

    Update models_profile.py

--- model_pages/models_profile.py
+++ model_pages/models_profile.py
@@ -1,4 +1,4 @@
-# 2026.07.23  18.00
+# 2026.08.10  17.00
 import dash
 from dash import html, dcc, callback, Input, Output, State, MATCH, ALL, ctx, no_update, clientside_callback
 import dash_bootstrap_components as dbc
