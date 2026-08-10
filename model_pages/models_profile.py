5aa985f0459d9d8fa9cbbaf58344edf3b731d3e2
fastautosol <fastautosol@gmail.com>
2026-08-10 17:21:00 +0200

    Update models_profile.py

--- model_pages/models_profile.py
+++ model_pages/models_profile.py
@@ -37,7 +37,7 @@ def layout(model_slug=None, **kwargs):
                 html.H2(model["name"], className="text-light fw-bold mb-1"),
                 html.P(model["niche"], className="text-info mb-1"),
                 html.Div(    
-                    html.A(f"{model['name']} your virtual girlfriend on Fanvue",  href=model["fanvue"], id="fanvue-link-btn",
+                    html.A(f"{model['name']}'s Private Club & DMs on Fanvue",  href=model["fanvue"], id="fanvue-link-btn",
                         target="_blank", rel="noopener noreferrer", className="btn btn-info btn-lg fw-bold", style={"width": "35%"}), 
                     className="text-center"),                                     
                 html.Span([html.I(className="fa-solid fa-users me-1"), f"{model['reach']} Reach"], className="badge bg-secondary text-light"),
