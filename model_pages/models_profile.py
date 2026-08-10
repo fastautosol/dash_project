a6913bdb74381dafca76227deabff669e23686ff
fastautosol <fastautosol@gmail.com>
2026-08-10 16:24:27 +0200

    Update models_profile.py

--- model_pages/models_profile.py
+++ model_pages/models_profile.py
@@ -37,17 +37,9 @@ def layout(model_slug=None, **kwargs):
                 html.H2(model["name"], className="text-light fw-bold mb-1"),
                 html.P(model["niche"], className="text-info mb-1"),
                 html.Div(    
-                    html.A(
-                        f"{model['name']} exclusive bikini photoset on Fanvue", 
-                        href=model["fanvue"], 
-                        id="fanvue-link-btn", # ID hozzáadva a követéshez
-                        target="_blank", 
-                        rel="noopener noreferrer", 
-                        className="btn btn-info btn-lg fw-bold", 
-                        style={"width": "30%"}
-                    ), 
-                    className="text-center"
-                ),                                     
+                    html.A(f"{model['name']} your virtual girlfriend on Fanvue",  href=model["fanvue"], id="fanvue-link-btn",
+                        target="_blank", rel="noopener noreferrer", className="btn btn-info btn-lg fw-bold", style={"width": "35%"}), 
+                    className="text-center"),                                     
                 html.Span([html.I(className="fa-solid fa-users me-1"), f"{model['reach']} Reach"], className="badge bg-secondary text-light"),
                 ], className="text-center py-4",
             ),
