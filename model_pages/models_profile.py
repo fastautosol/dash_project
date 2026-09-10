51a7ad1af306d132e3e3a05b38e7c076fb76eb9f
fastautosol <fastautosol@gmail.com>
2026-09-10 16:48:53 +0200

    Update models_profile.py

--- model_pages/models_profile.py
+++ model_pages/models_profile.py
@@ -1,4 +1,4 @@
-# 2026.09.10  15.00
+# 2026.09.10  16.00
 import dash
 from dash import html, dcc, callback, Input, Output, State, MATCH, ALL, ctx, no_update
 import dash_bootstrap_components as dbc
@@ -13,19 +13,9 @@ def layout(model_slug=None, **kwargs):
         return dbc.Container([
             html.H3("Model not found", className="text-light text-center mt-5"),
 
-                        html.Div(
-                dcc.Link(
-                    [
-                        html.I(className="fa-solid fa-arrow-left-long me-2"), 
-                        "Back to all models"
-                    ], 
-                    href="/", 
-                    className="back-btn-modern" 
-                ),
-                className="mb-2" 
-            ),
-
-         
+            html.Div(dcc.Link(
+                [html.I(className="fa-solid fa-arrow-left me-2"), "Back to all models"],  href="/",  className="back-btn-modern"), className="mb-2"),
+  
         ], className="py-3")
 
     if model["photos"]:
