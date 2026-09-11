51936ac590241ba31931e602ff4d59c7cf625850
fastautosol <fastautosol@gmail.com>
2026-09-11 21:02:34 +0200

    Update models_profile.py

--- model_pages/models_profile.py
+++ model_pages/models_profile.py
@@ -38,7 +38,7 @@ def layout(model_slug=None, **kwargs):
                 [
                 html.P(model["niche"], className="text-info mb-1"),
                 html.Div(    
-                    html.A(f"{model['name']}'s Exclusive content",  href=model["fanvue"], id="fanvue-link-btn",
+                    html.A(f"{model['name']}'s exclusive content",  href=model["fanvue"], id="fanvue-link-btn",
                         target="_blank", rel="noopener noreferrer", className="btn btn-info btn-lg fw-bold", style={"width": "80%"}), className="text-center"),                                     
                 ], className="text-center pb-4",
             ),
